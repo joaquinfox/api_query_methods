0ebb6bd8c165c0d53914e98f5e8eae77121aab0d
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-06-25 19:32:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const h4El = document.querySelector('h4');
+const imageEl = document.querySelector('img');
+const locationSpan = document.querySelector('#user-location');
+
+fetch(`https://randomuser.me/api/`)
+  .then((response) => response.json())
+  .then((response) => {
+    const userData = response.results[0];
+    const userName = userData.name;
+    const userLocation = userData.location;
+    const userPicture = userData.picture;
+    h4El.append(`${userName.title} ${userName.first} ${userName.last}`);
+    imageEl.src = userPicture.medium;
+    locationSpan.append(`${userLocation.city}, ${userLocation.country}`);
+  });
